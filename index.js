04f25fb4443bb3537da5378b70373a4d31189152
Gustav Burchardt <gustav@gbur.dk>
2021-03-18 14:40:29 +0100

    Publish 0.0.1

---
+++ index.js
@@ -0,0 +1,50 @@
+function createPredicateReg (options) {
+  const optionString = Object.keys(options).join('|') || '\\d+'
+  return new RegExp(`^((?:(?:${optionString}|\\d+)\\s*\\+\\s*)*(?:${optionString}|\\d+))\\s*(<=|>=|<|>)\\s*(${optionString}|\\d+)\\s*$`)
+}
+
+/**
+ * Parses predicate strings
+ * @param {String} query The predicate string that should be parsed
+ * @param {Object} options Optional options object, where keys can be accessed in the predicate
+ * @returns {Boolean} Is the predicate true?
+ */
+function parsePredicate (query, options = {}) {
+  const reg = createPredicateReg(options)
+  if (!reg.test(query)) {
+    throw new Error('Not valid')
+  }
+
+  const [, left, operand, right] = query.match(reg)
+
+  function mapPart (part) {
+    return options[part] || parseInt(part, 10)
+  }
+
+  const expectedValue = mapPart(right)
+
+  const sum = left
+    .split(/\s*\+\s*/)
+    .map(mapPart)
+    .reduce(
+      (acc, part) => acc + part,
+      0
+    )
+
+  switch (operand) {
+    case '<':
+      return sum < expectedValue
+    case '>':
+      return sum > expectedValue
+    case '<=':
+      return sum <= expectedValue
+    case '>=':
+      return sum >= expectedValue
+    default:
+      throw new Error('Operand unknown: ' + operand)
+  }
+}
+
+module.exports = {
+  parsePredicate
+}
